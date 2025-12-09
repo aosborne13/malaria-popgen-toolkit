05d32a4cee69a2b7f5f0ef657b77ff326118dfdc
Ashley Osborne <57442176+aosborne13@users.noreply.github.com>
2025-12-09 10:45:58 +0100

    Update __init__.py

--- src/malaria_popgen_toolkit/commands/__init__.py
+++ src/malaria_popgen_toolkit/commands/__init__.py
@@ -1,1 +1,6 @@
 # Makes 'commands' a Python package
+from . import missense_drugres_af
+from . import haplotype_map_region
+from . import fws_dotplot
+from . import pca_plot
+from . import hmmibd_matrix
